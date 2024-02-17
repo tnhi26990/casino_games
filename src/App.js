2e38a73812810dbb92dc910a4e39775f3751ccc3
Nhi Tran <trannhi@trans-air.home>
2024-02-17 11:19:13 -0600

    Fixed error, home page and game room not connected

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Gameroom from "./pages/Gameroom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+/*import { BrowserRouter, Routes, Route } from "react-router-dom";*/
 
 function App() {
   return (
