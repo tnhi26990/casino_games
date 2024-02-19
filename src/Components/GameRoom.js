d5874d1ef40954e158c2cd2a70b944b1c2ab00e5
Amar Hadzic <amarh@Amars-MacBook-Pro.local>
2024-02-19 13:44:20 -0600

    reorganized some files and code

--- src/Components/GameRoom.js
+++ src/Components/GameRoom.js
@@ -1,14 +1,18 @@
 import React from "react";
 
-
 function GameRoom() {
     return (
-        <div style={{ display: 'flex', flexDirection: 'row', width: '100vw', height: '100vh', justifyContent: 'space-between', alignItems: 'center' }}>
-            <button style={{ flex: 1, height: '50px' }}>Coin Flip</button>
-            <button style={{ flex: 1, height: '50px' }}>Roulette</button>
-            <button style={{ flex: 1, height: '50px' }}>Mines</button>
-        </div>
+        <>
+            <div className="page-room">
+                <h1 className="user-credits">Credits: </h1>
+                <div className = "game-btn-container">
+                    <button id = "mines-btn" className="game-btn" >Mines</button>
+                    <button id = "coin-btn" className="game-btn" >Coin Flip</button>
+                    <button id = "roulette-btn" className="game-btn" >Roulette</button>
+                </div>
+            </div>
 
+        </>
     )
 
 }
--- src/pages/Gameroom.js
+++ src/pages/Gameroom.js
@@ -1,16 +1,11 @@
-import Header from "../Components/Header";
+import GameRoom from "../Components/GameRoom";
 import "../Buttons.css";
+import "../GameRoom.css"
 
 export default function() {
     return (
         <>
-            <div className="page-room">
-                <div className = "game-btn-container">
-                    <button id = "mines-btn" className="game-btn" >Mines</button>
-                    <button id = "coin-btn" className="game-btn" >Coin Flip</button>
-                    <button id = "roulette-btn" className="game-btn" >Roulette</button>
-                </div>
-            </div>
+            <GameRoom/>
 
         </>
     )
