8ff9faa8b25df86028a441497bb65a08480aa384
Adem Durakovic <adem.durakovic@slu.edu>
2024-02-22 16:33:02 -0600

    player negative bets fixed

--- back_end/Player.cpp
+++ back_end/Player.cpp
@@ -1,16 +1,41 @@
+#include "Player.h"
+#include <iostream>
+#include "Casino.h"
 
-class Player{
-private:
-    int credits = 5000;
+// Constructor definition
+Player::Player() : credits(5000), casino(nullptr), currentBet(0) {}
 
-public:
-    int placeBet(int bet){
-        if ( credits >= bet){
-            return bet;
-        }
-        return 0;
+// Destructor definition
+Player::~Player() {
+    // Perform any cleanup here if needed
+}
+
+// Other member function definitions remain unchanged
+void Player::registerCasino(Casino* casinoObs) {
+    casino = casinoObs;
+}
+
+void Player::playRound(bool prediction) {
+    if (currentBet != 0) {
+        casino->update(currentBet, prediction);
+        this->currentBet = 0;
     }
+}
 
+void Player::increaseBet(int bet) {
+    if (bet > credits || bet <= 0) {
+        std::cout << "You do not have enough for this bet." << std::endl;
+    } else {
+        currentBet += bet;
+        credits -= bet;
+    }
+}
 
+void Player::updateCredits(int money) {
+    this->credits += money;
+}
 
-};
+void Player::printCredits() {
+    std::cout << "You have " << this->credits << " credits." << std::endl;
+}
+
