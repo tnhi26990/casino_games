e8e878cd8423989353d996abf640be1e9f553106
Amar Hadzic <amarh@amars-mbp.attlocal.net>
2024-03-17 13:04:35 -0500

    resolving issue 10 and redesigned homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,13 @@
 import Header from "../Components/Header";
 import Home from "../Components/Home";
-import Footer from "../Components/Footer";
+import "../HomePage.css"
 
 export default function() {
     return (
       <>
         <Header />
         <Home />
-        <Footer />
+        
       </>
     );
   }
