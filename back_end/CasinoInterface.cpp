47e585d4c7f993abb7a0c3b6079a0dcba3d1cd64
Nhi Tran <trannhi@Trans-MacBook-Air.local>
2024-04-13 14:32:03 -0500

    delete all back end code

--- back_end/CasinoInterface.cpp
+++
@@ -1,6 +0,0 @@
-
-class CasinoInterface {
-public:
-    virtual void update(int bet, bool prediction) = 0; // Pure virtual function
-    //virtual bool checkWinner(bool prediction, bool outcome) = 0; // Pure virtual function
-};
--- back_end/cmake-build-debug/CMakeFiles/back_end.dir/CasinoInterface.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/back_end.dir/CasinoInterface.cpp.o: \
-  /Users/adem/testingAPI/CSCI3300_Group6/back_end/CasinoInterface.cpp
