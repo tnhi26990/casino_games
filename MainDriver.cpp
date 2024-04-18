6aa2dcb0aae33bcbd3e47f4bac2355fb7c2e4dd8
Adem Durakovic <adem.durakovic@slu.edu>
2024-04-18 15:43:53 -0500

    coinFlip model now in back end

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -3,6 +3,9 @@
 #include "Casino.h"
 #include <string>
 #include "App.h"
+#include "CoinGame.h"
+#include <typeinfo>
+
 
 using namespace std;
 
@@ -10,8 +13,9 @@ int main() {
     Player* player = new Player();
     string playerBalance = "coinFlip";
     string game = "coin";
-    float amount = 0.0f; // Declare amount as float
+    int amount = 0; // Declare amount as int
     string amountToFront = "";
+    CoinGame* coinGame = new CoinGame();
 
     struct PerSocketData {
         /* Fill with user data */
@@ -21,21 +25,50 @@ int main() {
             .open = [](auto *ws) {
                 std::cout << "Client connected" << std::endl;
             },
-            .message = [&playerBalance, &game, &amount, &player, &amountToFront](auto *ws, std::string_view message, uWS::OpCode opCode) {
+            .message = [&coinGame, &playerBalance, &game, &amount, &player, &amountToFront](auto *ws, std::string_view message, uWS::OpCode opCode) {
                 std::cout << "Received message from client: " << message << std::endl;
 
-              {
-                    try {
-                        amount = std::stof(std::string(message)); // Convert string to float
-                        player->updateCredits(amount);
-                    } catch (const std::invalid_argument &e) {
-                        std::cerr << "Invalid argument: " << e.what() << std::endl;
-                        // Handle invalid argument error
-                    } catch (const std::out_of_range &e) {
-                        std::cerr << "Out of range: " << e.what() << std::endl;
-                        // Handle out of range error
+                try{
+                    // Find the position of the space
+                    size_t pos = message.find(' ');
+                    if (pos == std::string::npos) {
+                        std::cerr << "Invalid message format" << std::endl;
+                        return;
+                    }
+                    std::string type = std::string(message.substr(0, pos));  //guess
+                    int value = std::stoi(std::string(message.substr(pos + 1)));  //bet amount
+                    cout<<type << " " << value <<endl;
+
+                    //setting user prediction
+                    bool prediciton;
+                    if (type == "T"){ prediciton = true;}
+                    else{ prediciton = false;}
+
+                    bool outcome = coinGame->executeRound(prediciton); //flip coin and check winner
+                    //send the code that matches with either h or t
+                    if (outcome == true){
+                        if (prediciton == true){
+                            ws->send("1", uWS::OpCode::TEXT); //they chose tails, won so we send back tails
+                        }
+                        else{
+                            ws->send("0", uWS::OpCode::TEXT);
+                        }
+                        player->updateCredits(value);
+                    }
+                    else{
+                        if (prediciton == true){ //they chose heads, lost so we send back tails
+                            ws->send("0", uWS::OpCode::TEXT);
+                        }
+                        else{
+                            ws->send("1", uWS::OpCode::TEXT);
+                        }
+                        player->updateCredits(-value);
                     }
                 }
+                catch(const std::exception& e){
+                    cout<<"Just user asking for credits at start"<<endl;
+                }
+
                 amount = player->getCredits();
                 amountToFront = to_string(amount);
                 ws->send(amountToFront, uWS::OpCode::TEXT);
@@ -52,6 +85,7 @@ int main() {
     }).run();
 
     delete player;
+    delete coinGame;
 
     return 0;
 }
