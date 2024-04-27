44c14b5750ee5deb01f1eaf7bb0a77b0f6232150
Nhi Tran <trannhi@Trans-MacBook-Air.local>
2024-04-27 09:19:36 -0500

    pull changes from main branch

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -9,12 +9,20 @@
 
 using namespace std;
 
+bool startsWith(const std::string_view str, const std::string_view prefix) {
+    return str.size() >= prefix.size() && str.substr(0, prefix.size()) == prefix;
+}
+
 int main() {
     Player* player = new Player();
     string playerBalance = "coinFlip";
-    string game = "coin";
-    float amount = 0.0f; // Declare amount as float
+    string game = "roulette";
+    int currentPayout = 0;
+    int amount = 0;
     string amountToFront = "";
+    RouletteWheel* rouletteWheel = new RouletteWheel();
+    BettingTable* rouletteGame = new BettingTable(*rouletteWheel);
+    cout<< "starting credits : " << player->getCredits() <<endl;
 
     struct PerSocketData {
         /* Fill with user data */
@@ -24,27 +32,38 @@ int main() {
             .open = [](auto *ws) {
                 std::cout << "Client connected" << std::endl;
             },
-            .message = [&playerBalance, &game, &amount, &player, &amountToFront](auto *ws, std::string_view message, uWS::OpCode opCode) {
-                std::cout << "Received message from client: " << message << std::endl;
+            .message = [&rouletteWheel, &rouletteGame, &player,&currentPayout, &amountToFront, &game](auto *ws, std::string_view message, uWS::OpCode opCode) {
+                    std::cout << "Received message from client: " << message << std::endl;
 
-              {
-                    try {
-                        amount = std::stof(std::string(message)); // Convert string to float
-                        player->updateCredits(amount);
-                    } catch (const std::invalid_argument &e) {
-                        std::cerr << "Invalid argument: " << e.what() << std::endl;
-                        // Handle invalid argument error
-                    } catch (const std::out_of_range &e) {
-                        std::cerr << "Out of range: " << e.what() << std::endl;
-                        // Handle out of range error
-                    }
+                if (message.substr(0,18) == "Button was clicked"){
+                    cout<<"button clicked"<<endl;
+                    std::string chipVal((message.substr(19)));
+                    cout<<chipVal<<endl;
+                    int chipValInt = stoi(chipVal); //converting str to int
+                    player->updateCredits(-chipValInt);
+                    amountToFront = std::to_string(player->getCredits());
+                    ws->send(amountToFront , uWS::OpCode::TEXT);
                 }
-                amount = player->getCredits();
-                amountToFront = to_string(amount);
-                ws->send(amountToFront, uWS::OpCode::TEXT);
-                cout << player->getCredits() << endl; // Corrected syntax
-            },
+                else if (message == "hi"){
 
+                }
+                else{
+                    std::string messageStr(message);
+                        int spinRes = rouletteWheel ->generateNumber();
+                        cout<<"Spin result is : " << spinRes <<endl;
+                        int payout = rouletteGame ->executeRound(messageStr, spinRes);
+                        cout<< "player payout is:" << payout << endl;
+                        player->updateCredits(payout);
+                        spinRes -=1;
+                        ws->send(to_string(spinRes )+ " " + "slot", uWS::OpCode::TEXT);
+                    }
+        // Update the front end with the new credits amount
+                cout<< "current credits : " << player->getCredits() <<endl;
+                amountToFront = std::to_string(player->getCredits());
+                ws->send(amountToFront, uWS::OpCode::TEXT);
+
+
+    },
             .close = [](auto *ws, int code, std::string_view message) {
                 std::cout << "Client disconnected" << std::endl;
             }
@@ -55,6 +74,9 @@ int main() {
     }).run();
 
     delete player;
+    delete rouletteGame;
+    delete rouletteGame;
 
     return 0;
 }
+
